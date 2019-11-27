fdbd36fa775709af8e67a149d5aca28360679434
Pablo de Haro <pablitar@gmail.com>
2019-11-27 10:28:36 -0300

    chore: static build

---
+++ precache-manifest.4d32f96e3bfb828252c65f921e37d728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0dc6fa64cf67c022081fc19c4d2142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92dbdfe4a5e3f7ee7c9b",
+    "url": "/static/css/main.06244c5a.chunk.css"
+  },
+  {
+    "revision": "4641ad579eca823a7895",
+    "url": "/static/js/2.23c7531c.chunk.js"
+  },
+  {
+    "revision": "6eb650f7fade39be2f8d33a791405888",
+    "url": "/static/js/2.23c7531c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dbdfe4a5e3f7ee7c9b",
+    "url": "/static/js/main.0d63d7ed.chunk.js"
+  },
+  {
+    "revision": "8986d5edfdf93656da0d",
+    "url": "/static/js/runtime-main.5a35bbcb.js"
+  }
+]);
